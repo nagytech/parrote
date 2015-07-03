528dd61734cf9cc7707db584ae5d024bbe2b21ef
nagytech <nagytech@users.noreply.github.com>
2015-07-03 20:33:42 +1000

    add glyphicons relative, use messages

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -3,11 +3,13 @@ package controllers;
 import play.mvc.Controller;
 import play.mvc.Result;
 
+import views.html.*;
+
 public class Application extends Controller {
 
     public static Result index() {
 
-        return ok(views.html.index.render());
+        return ok(index.render());
 
     }
 
